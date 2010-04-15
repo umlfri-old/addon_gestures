bad1442c687b37794eec19963939382271683c02
pasikavec <pasikavec@d6d51e68-c4c5-48db-96ef-4d935770b557>
2010-04-15 20:31:14 +0000

    Now user is able to paint gestures inside elements, for example in Boundary, Package ...

--- gestures/trunk/plugin/gestureLogic/GestureManager.py
+++ gestures/trunk/plugin/gestureLogic/GestureManager.py
@@ -2,6 +2,7 @@ import os.path
 from lxml import etree
 import xml.etree.ElementTree as etree
 from BoundaryAlgorithm import CBoundaryAlgorithm
+import math
 
 class CGestureManager(object):
     def __init__(self):
@@ -9,42 +10,44 @@ class CGestureManager(object):
         self.coord = []
         self.alg = CBoundaryAlgorithm()
         self.dic = []        
+        self.deleteConnectionCount = 8
+        self.deleteConnectionDistance = 10
+        self.result = []
         
+    #Pomocna metoda pri rozliseni typu gesta
     def FindOutGestureType(self):
-        self.type = ""
-        pocet = 0
-        pocNE = 0
+        isHigher = False
         pocC = 0
-        self.coordinatesC = []
-        print self.helpCoord
-        for i in self.helpCoord:                        
-            if i[2] != 'N':
-                pocet = pocet+1
-            if i[2] != 'AE':
-                pocNE = pocNE+1
+        self.deletePoints = []
+        spojenie = False
+        for i in self.helpCoord:
             if i[2] == 'AC':
-                self.coordinatesC.append(i);
-                pocC = pocC + 1
-            
-        if pocet == len(self.helpCoord):            
-            self.type = 'delete element'        
-                
-        if pocNE == len(self.helpCoord) and pocC >=2:
-            self.type = 'delete connection'
-            return
-        
-        if self.type !='delete element':
-            for i in self.helpCoord:                    
-                if i[2] == 'X':
-                    self.type = 'connection'                
-                    self.helpCoord.insert(self.helpCoord.index(i)+1, ['x','x','x'])
-                    for j in self.helpCoord:                                
-                        if j[2] == 'P':
-                            self.helpCoord.insert(self.helpCoord.index(j)+1, ['p','p','p'])                                                                
-                    break                   
-            if self.type !='connection':
-                self.type = 'element or system'
-        print self.type
+                self.deletePoints.append(i)
+        if len(self.deletePoints) >= self.deleteConnectionCount:            
+            for i in range(len(self.deletePoints)):
+                for j in range(len(self.deletePoints)):
+                    roz = math.sqrt(math.fabs(self.deletePoints[i][0]-self.deletePoints[j][0])+
+                          math.fabs(self.deletePoints[i][1]-self.deletePoints[j][1]))
+                    if roz > self.deleteConnectionDistance:
+                        isHigher = True
+            if isHigher == False:
+                self.result.append('delete connection')
+                self.result.append([self.deletePoints[0][0],self.deletePoints[0][1]])
+                return        
+        for i in self.helpCoord:
+            if i[2] == 'V':
+                spojenie = True        
+        if spojenie == False:
+            self.result.append('element')
+            return                                                                                         
+        for i in self.helpCoord:                    
+            if i[2] == 'X':
+                self.result.append('connection')                
+                self.helpCoord.insert(self.helpCoord.index(i)+1, ['x','x','x'])
+                for j in self.helpCoord:                                
+                    if j[2] == 'P':
+                        self.helpCoord.insert(self.helpCoord.index(j)+1, ['p','p','p'])                                                                
+                break                   
                          
     def SetCoord(self,pix):
         self.helpCoord = pix
@@ -52,6 +55,7 @@ class CGestureManager(object):
     def CreateCoordinates(self):
         for i in self.helpCoord:
             self.coord.append((i[0],i[1]))
+        print self.coord
         self.alg.SetCoordinates(self.coord)        
     
     def DeleteCoordinates(self):
@@ -79,14 +83,20 @@ class CGestureManager(object):
                         i[1] = gest    
     
     def Recognize(self):
+        self.result = []        
         self.FindOutGestureType()
+        print self.result
+        if self.result[0] == 'delete connection':
+            return self.result
         self.CreateCoordinates()
-       # print self.dic                
-        a = self.alg.Recognition(self.type)
+        a = self.alg.Recognition(self.result)
+        if a[0] == 'unknown':
+            return a
+        if a[0] == 'system':
+            return a
         if a[0] == 'delete element':
-            return a        
-        if a[0] == 'delete connection':
-            a.append(self.coordinatesC[0])
+            if self.helpCoord[0][2] != 'AE':
+                a[0] = 'unknown'             
             return a
         if a[0] == 'element':
             bool = False
@@ -96,21 +106,16 @@ class CGestureManager(object):
                     a.append(('Element',i[0]))
                     return a
             if bool == False:
-                del a[:]
-                a.append('error')
-                return a            
+                a[0] = 'unknown'             
+                return a    
         if a[0] == 'connection':
             bool = False
             for i in self.dic:
                 if i[1]==a[1]:
-                    bool = True                    
+                    bool = True           
+                    print a         
                     a.append(('Connection',i[0]))
                     return a
             if bool == False:
-                del a[:]
-                a.append('error')
-                return a            
-        #del a[:]                 
-        #a.append('error')
-        return a
-    
+                a[0] = 'unknown'             
+                return a    
